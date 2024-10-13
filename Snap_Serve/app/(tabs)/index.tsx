c84b94e4c7c6c621974635aa0e62b73ee7700094
YoyoJesus <austinplaysmms12@gmail.com>
2024-10-12 23:31:32 -0400

    imagine

--- Snap_Serve/app/(tabs)/index.tsx
+++ Snap_Serve/app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/food_banner.jpg')}
+          source={require('@/assets/images/soda_banner.jpg')}
           style={styles.reactLogo}
         />
       }>
